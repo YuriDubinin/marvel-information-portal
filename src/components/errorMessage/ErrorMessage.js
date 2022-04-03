3e4c5649e9caad323814404d33de30329d30732f
Yura Dubinin <yuradubinin100@gmail.com>
2022-04-03 16:05:11 +0700

    Added ErrorMessage & Spinner components
    * In RandomChar component added new state field 'error'
    
    * RandomChar component now has View - render component
    
    * Added new method onError (in RandomChar component)
    
    * RandomChar - main component, View - render component
    
    * All the logic that happens in the RandomChar component happens in the render method

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,15 @@
+import img from "./error.gif";
+
+const ErrorMessague = () => {
+    return (
+        <div className="error-message">
+            <img
+                style={{ display: "block", width: "250px", height: "250px", objectFit: "contain", margin: "0 auto" }}
+                src={img}
+                alt="error message"
+            />
+        </div>
+    );
+};
+
+export default ErrorMessague;
