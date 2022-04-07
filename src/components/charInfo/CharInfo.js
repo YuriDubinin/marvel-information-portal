3feb930977fc09ed1fa238d59825e2e5e6dbd087
Yura Dubinin <yuradubinin100@gmail.com>
2022-04-07 20:32:42 +0700

    Type checking with PropTypes
    * Installed npm package prop-types
    
    * Prop type checking added to CharInfo & Charlist  components
    
    * CharInfo: minor style changes

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import PropTypes from "prop-types";
 
 import MarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spiner";
@@ -116,6 +117,11 @@ const View = ({ char }) => {
             </ul>
         </>
     );
+};
+
+//type checking
+CharInfo.propTypes = {
+    charId: PropTypes.number,
 };
 
 export default CharInfo;
--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import PropTypes from "prop-types";
 
 import MarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spiner";
@@ -110,5 +111,10 @@ class CharList extends Component {
         );
     }
 }
+
+//type checking
+CharList.propTypes = {
+    onCharSelected: PropTypes.func.isRequired,
+};
 
 export default CharList;
