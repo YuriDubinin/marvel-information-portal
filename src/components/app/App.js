2152d53c3f6274fd90b698b47f1f151838eb22a9
Yura Dubinin <yuradubinin100@gmail.com>
2022-04-22 09:59:53 +0700

    Added React Router v5
    * Added 2 components MainPage & ComicsPage
    
    * Added index.js (in pages folder) for easy page import
    
    * Refactoring component App
    
    * There is incorrect behavior when switching pages due to the 18 version of react

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,45 +1,27 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import { MainPage, ComicsPage } from "../pages";
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-import ComicsList from "../comicsList/ComicsList";
-
-import decoration from "../../resources/img/vision.png";
 
 const App = () => {
-    const [selectedChar, setChar] = useState(null);
-
-    const onCharSelected = (id) => {
-        setChar(id);
-    };
-
     return (
-        <div className="app">
-            <AppHeader />
+        <Router>
+            <div className="app">
+                <AppHeader />
 
-            <main>
-                <ErrorBoundary>
-                    <RandomChar />
-                </ErrorBoundary>
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected} />
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar} />
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision" />
-            </main>
+                <main>
+                    <Switch>
+                        <Route exact path="/">
+                            <MainPage />
+                        </Route>
 
-            {/* testing comics list */}
-            <ErrorBoundary>
-                <ComicsList />
-            </ErrorBoundary>
-        </div>
+                        <Route exact path="/comics">
+                            <ComicsPage />
+                        </Route>
+                    </Switch>
+                </main>
+            </div>
+        </Router>
     );
 };
 
