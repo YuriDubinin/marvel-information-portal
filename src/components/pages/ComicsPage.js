de9d5037b77e26d9fff163f54a1f87ed80f89c12
Yura Dubinin <yuradubinin100@gmail.com>
2022-05-12 20:24:46 +0700

    SEO optimization (react-helmet)
    * Added 'react-helmet' dependency to the project
    
    * All required pages now get title and meta tag
    
    * In some cases, dynamic shaping is used

--- src/components/pages/ComicsPage.js
+++ src/components/pages/ComicsPage.js
@@ -1,9 +1,15 @@
+import { Helmet } from "react-helmet";
+
 import AppBanner from "../appBanner/AppBanner";
 import ComicsList from "../comicsList/ComicsList";
 
 const ComicsPage = () => {
     return (
         <>
+            <Helmet>
+                <meta name="description" content="List of comics based on the Marvel Universe" />
+                <title>Marvel comics</title>
+            </Helmet>
             <AppBanner />
             <ComicsList />
         </>
--- src/components/pages/SingleCharPageLayout/SingleCharPage.js
+++ src/components/pages/SingleCharPageLayout/SingleCharPage.js
@@ -1,10 +1,15 @@
 import "./singleCharPage.scss";
+import { Helmet } from "react-helmet";
 
 const SingleCharPage = ({ data }) => {
     const { name, description, thumbnail } = data;
 
     return (
         <div className="single-char">
+            <Helmet>
+                <meta name="description" content={`'${name}' profile`} />
+                <title>{name}</title>
+            </Helmet>
             <img src={thumbnail} alt={name} className="single-char__img" />
             <div className="single-char__info">
                 <h2 className="single-char__name">{name}</h2>
